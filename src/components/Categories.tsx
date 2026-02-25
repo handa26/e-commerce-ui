b851322a19a17a9f81339b48beb627e7aa41bec1
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-02-25 19:55:02 +0800

    feat: categories search params when clicking

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -54,12 +54,23 @@ const categories = [
 ];
 
 const Categories = () => {
+	const searchParams = useSearchParams();
+	const router = useRouter();
+	const pathname = usePathname();
+
+	const handleChange = (value: string | null) => {
+		const params = new URLSearchParams(searchParams);
+		params.set("category", value || "");
+		router.push(`${pathname}?${params.toString()}`, { scroll: false });
+	};
+
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm">
 			{categories.map((category) => (
 				<div
-					className="flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md"
+					className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${category.slug === searchParams.get("category") ? "bg-white" : "text-gray-500"}`}
 					key={category.name}
+					onClick={() => handleChange(category.slug)}
 				>
 					{category.icon}
 					{category.name}
