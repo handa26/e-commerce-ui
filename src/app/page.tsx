3c241739d600409fc1696c8467013409230310fb
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-02-25 19:38:23 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+import ProductList from "@/components/ProductList";
+
+const Homepage = () => {
+	return (
+		<div className="">
+			<div className="relative aspect-[3/1] mb-12">
+				<Image src="/featured.png" alt="Featured Product" fill />
+			</div>
+
+			<ProductList />
+		</div>
+	);
+};
+
+export default Homepage;
