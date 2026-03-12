0365112748d7f0320d135d82b5fcc4131aa529d2
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-03-12 20:00:10 +0800

    feat: payment form design with validation

--- src/types.ts
+++ src/types.ts
@@ -34,3 +34,17 @@ export const shippingFormSchema = z.object({
 });
 
 export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+export const paymentFormSchema = z.object({
+	cardHolder: z.string().min(3, "Card holder is required!"),
+	cardNumber: z
+		.string()
+		.min(16, "Card number is required!")
+		.max(16, "Card number is required!"),
+	expirationDate: z
+		.string()
+		.regex(/^(0[1-9]|1[0-2])\/\d{2}$/, "Expiration date is required!"),
+	cvv: z.string().min(3, "CVV is required!").max(3, "CVV is required!"),
+});
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
